9234cb357d20a88ed2886b8717e232ee138ff80c
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-10-12 17:09:32 -0300

    mudei para tabuada

---
+++ script.js
@@ -0,0 +1,13 @@
+const primeiroInput = document.querySelector('#input1')
+const segundoInput = document.querySelector('#input2')
+const botao = document.querySelector('#btn')
+const resultado = document.querySelector('#resultado')
+const container = document.querySelector('.container')
+
+function adicionar () {
+    for(let i = 0; i <= segundoInput.value; i++){
+        
+    }
+}
+
+botao.addEventListener('click',  adicionar)
